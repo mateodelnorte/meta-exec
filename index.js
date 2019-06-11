c286c5bbc9ea531fd53ae6e013f8c92de1b93f50
Matt Walters <github@iammattwalters.com>
2019-06-11 13:26:25 -0500

    chore: standardizing test, linting, and commit messages

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const debug = require('debug')('meta-exec');
 const execSync = require('child_process').execSync;
 const path = require('path');
 
-module.exports = function (options, cb, errorCb) {
-
+module.exports = function(options, cb, errorCb) {
   if (options.stdio === undefined) options.stdio = [0, 1, 2];
   if (options.suppressLogging === undefined) options.suppressLogging = false;
   if (options.suppressLogging) options.stdio[1] = 'ignore';
@@ -18,8 +17,7 @@ module.exports = function (options, cb, errorCb) {
   var code = null;
 
   try {
-    if (!options.suppressLogging)
-      console.log(`\n${chalk.cyan(path.basename(options.displayDir))}:`);
+    if (!options.suppressLogging) console.log(`\n${chalk.cyan(path.basename(options.displayDir))}:`);
 
     code = execSync(options.cmd, {
       cwd: options.dir,
@@ -27,37 +25,35 @@ module.exports = function (options, cb, errorCb) {
       stdio: options.stdio,
     });
   } catch (err) {
-
     if (errorCb) errorCb(err);
-    
+
     // if there is no error callback, we're just going to forward the output
-    let errorMessage = `${chalk.red(path.basename(options.displayDir))}: command '${options.cmd}' exited with error: ${err.toString()}`;
-    
-    if ( ! options.suppressLogging) console.error(errorMessage);
+    let errorMessage = `${chalk.red(path.basename(options.displayDir))}: command '${
+      options.cmd
+    }' exited with error: ${err.toString()}`;
+
+    if (!options.suppressLogging) console.error(errorMessage);
 
     if (cb) return cb(null, { error: errorMessage });
 
     return;
-
   }
 
   if (code) {
-
     let errorMessage = `${chalk.red(path.basename(options.displayDir))} '${options.cmd}' exited with code: ${code}`;
 
     if (errorCb) errorCb(new Error(errorMessage));
-    
-    if ( ! options.suppressLogging) console.error(errorMessage);
+
+    if (!options.suppressLogging) console.error(errorMessage);
 
     if (cb) return cb(null, { err: errorMessage });
 
     return;
-  } 
+  }
 
   let success = chalk.green(`${path.basename(options.displayDir)} ✓`);
 
-  if ( ! options.suppressLogging) console.log(success);
-  
-  if (cb) return cb(null, { output: success });
+  if (!options.suppressLogging) console.log(success);
 
+  if (cb) return cb(null, { output: success });
 };
