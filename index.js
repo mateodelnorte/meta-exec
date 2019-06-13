96cd0b5d15b1bc03495996be3d8ef2306d49aed1
Matt Walters <github@iammattwalters.com>
2019-06-13 01:33:27 -0500

    feat: adding parallel execution via parallel loop argv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 const debug = require('debug')('meta-exec');
-const execSync = require('child_process').execSync;
+const cp = require('child_process');
 const path = require('path');
 
 module.exports = function(options, cb, errorCb) {
@@ -8,52 +8,100 @@ module.exports = function(options, cb, errorCb) {
   if (options.suppressLogging === undefined) options.suppressLogging = false;
   if (options.suppressLogging) options.stdio[1] = 'ignore';
 
+  options.cmd = options.cmd || options.command;
   options.dir = options.dir || process.cwd();
   options.displayDir = options.displayDir || options.dir;
-  options.cmd = options.cmd || options.command;
+  options.parallel = options.parallel || false;
 
   debug(`executing command ${options.cmd} in dir ${options.dir}`);
 
   var code = null;
 
-  try {
-    if (!options.suppressLogging) console.log(`\n${chalk.cyan(path.basename(options.displayDir))}:`);
+  const execPromise = options.parallel
+    ? new Promise((resolve, reject) => {
+        cp.exec(
+          options.cmd,
+          {
+            cwd: options.dir,
+            env: process.env,
+            stdio: options.stdio,
+          },
+          (err, stdout, stderr) => {
+            if (err) return reject(err);
+            if (err) {
+              let errorMessage = `${chalk.red(path.basename(options.displayDir))} '${
+                options.cmd
+              }' exited with code: ${err}`;
 
-    code = execSync(options.cmd, {
-      cwd: options.dir,
-      env: process.env,
-      stdio: options.stdio,
+              if (errorCb) errorCb(new Error(errorMessage));
+
+              if (!options.suppressLogging) console.error(errorMessage);
+
+              if (cb) return cb(null, { err: errorMessage });
+
+              return;
+            }
+
+            if (!options.suppressLogging) console.log(`\n${chalk.cyan(path.basename(options.displayDir))}:`);
+
+            stdout.length && console.log(stdout.trim());
+            stderr.length && console.log(stderr.trim());
+
+            let success = chalk.green(`${path.basename(options.displayDir)} ✓`);
+
+            if (!options.suppressLogging) console.log(success);
+            return resolve(err ? 1 : 0);
+          }
+        );
+      })
+    : new Promise((resolve, reject) => {
+        if (!options.suppressLogging) console.log(`\n${chalk.cyan(path.basename(options.displayDir))}:`);
+        try {
+          const code = cp.execSync(options.cmd, {
+            cwd: options.dir,
+            env: process.env,
+            stdio: options.stdio,
+          });
+          if (code) {
+            let errorMessage = `${chalk.red(path.basename(options.displayDir))} '${
+              options.cmd
+            }' exited with code: ${code}`;
+
+            if (errorCb) errorCb(new Error(errorMessage));
+
+            if (!options.suppressLogging) console.error(errorMessage);
+
+            if (cb) return cb(null, { err: errorMessage });
+
+            return;
+          }
+
+          let success = chalk.green(`${path.basename(options.displayDir)} ✓`);
+
+          if (!options.suppressLogging) console.log(success);
+
+          return resolve(success);
+        } catch (err) {
+          if (errorCb) errorCb(err);
+
+          // if there is no error callback, we're just going to forward the output
+          let errorMessage = `${chalk.red(path.basename(options.displayDir))}: command '${
+            options.cmd
+          }' exited with error: ${err.toString()}`;
+
+          if (!options.suppressLogging) console.error(errorMessage);
+
+          if (cb) return cb(null, { error: errorMessage });
+
+          return reject(1);
+        }
+      });
+
+  execPromise
+    .then(success => {
+      if (cb) return cb(null, { output: success });
+    })
+    .catch(errorMessage => {
+      if (cb) return cb(null, { error: errorMessage });
     });
-  } catch (err) {
-    if (errorCb) errorCb(err);
-
-    // if there is no error callback, we're just going to forward the output
-    let errorMessage = `${chalk.red(path.basename(options.displayDir))}: command '${
-      options.cmd
-    }' exited with error: ${err.toString()}`;
-
-    if (!options.suppressLogging) console.error(errorMessage);
-
-    if (cb) return cb(null, { error: errorMessage });
-
-    return;
-  }
-
-  if (code) {
-    let errorMessage = `${chalk.red(path.basename(options.displayDir))} '${options.cmd}' exited with code: ${code}`;
-
-    if (errorCb) errorCb(new Error(errorMessage));
-
-    if (!options.suppressLogging) console.error(errorMessage);
-
-    if (cb) return cb(null, { err: errorMessage });
-
-    return;
-  }
-
-  let success = chalk.green(`${path.basename(options.displayDir)} ✓`);
-
-  if (!options.suppressLogging) console.log(success);
-
-  if (cb) return cb(null, { output: success });
 };
